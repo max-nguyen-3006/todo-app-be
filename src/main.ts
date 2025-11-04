9193806aa552f5f64803142be911c5b20259c223
max-nguyen-3006 <manhkhacnguyen1997@gmail.com>
2025-11-04 11:26:31 +0700

    [DEV] add nest/config to read env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
   console.log("test");
+  console.log(process.env.NODE_ENV);
+  
   
 }
 bootstrap();
