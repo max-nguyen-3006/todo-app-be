785f49722c60579eecb9a0164942d4a031c134e1
max-nguyen-3006 <manhkhacnguyen1997@gmail.com>
2025-11-05 11:22:33 +0700

    [DEV] add cpu controller to test scaler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,12 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('cpu')
+  generateCpuLoad(): string {
+    const end = Date.now() + 10000;
+    while (Date.now() < end) {
+      Math.sqrt(Math.random()); 
+    }
+    return 'CPU load generated for 10 seconds!';
+  }
 }
